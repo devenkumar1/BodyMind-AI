55c5a9b03357371d153cdd822fd07f87f83f3083
devenkumar1 <devenkumar540@gmail.com>
2025-04-07 17:52:54 +0530

    fixed getAll Traine~r bug

--- frontend/src/pages/MyBookings.tsx
+++ frontend/src/pages/MyBookings.tsx
@@ -20,7 +20,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-// API URLs
+
 const API_BASE_URL = import.meta.env.VITE_API_URL ? 
   `${import.meta.env.VITE_API_URL}/api/user/training` : 
   'http://localhost:5000/api/user/training';
